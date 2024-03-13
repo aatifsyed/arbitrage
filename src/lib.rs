7cd1267c6e0bff67bf93db9e6be69f2a42b9b6b1
Aatif Syed <aatifsyedyp@gmail.com>
2024-03-13 13:55:33 +0000

    test: add some basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use num_traits::Zero;
 
 /// Keeps track of arbitrage opportunities across exchanges.
 #[derive(Debug, Clone)]
-pub struct Arbitrage<QuantityT, PriceT, ExchangeIdT, BuildHasherT = RandomState> {
+pub struct ArbitrageFinder<QuantityT, PriceT, ExchangeIdT, BuildHasherT = RandomState> {
     #[doc(alias = "buys")]
     bids: BTreeMap<PriceT, HashMap<ExchangeIdT, QuantityT, BuildHasherT>>,
     #[doc(alias = "sells")]
@@ -20,13 +20,18 @@ pub struct Arbitrage<QuantityT, PriceT, ExchangeIdT, BuildHasherT = RandomState>
 pub enum Error<ExchangeIdT> {
     /// An exchange needlessly stated that a price level was empty.
     ///
-    /// This does not signal that the [`Arbitrage`] is corrupt, but may indicate
+    /// This does not signal that the [`ArbitrageFinder`] is corrupt, but may indicate
     /// incoming data loss.
     Needless { exchange_id: ExchangeIdT },
 }
 
 impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
-    Arbitrage<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
+    ArbitrageFinder<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
+where
+    PriceT: Ord + Clone,
+    QuantityT: Zero,
+    ExchangeIdT: Eq + Hash,
+    BuildHasherT: BuildHasher + Default,
 {
     #[doc(alias = "bid")]
     pub fn buy(
@@ -34,13 +39,7 @@ impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
         exchange_id: ExchangeIdT,
         price: PriceT,
         quantity: QuantityT,
-    ) -> Result<impl Iterator<Item = (&PriceT, &ExchangeIdT, &QuantityT)>, Error<ExchangeIdT>>
-    where
-        PriceT: Ord + Clone,
-        QuantityT: Zero,
-        ExchangeIdT: Eq + Hash,
-        BuildHasherT: BuildHasher + Default,
-    {
+    ) -> Result<impl Iterator<Item = (&ExchangeIdT, &PriceT, &QuantityT)>, Error<ExchangeIdT>> {
         match quantity.is_zero() {
             false => {
                 insert(&mut self.bids, price.clone(), exchange_id, quantity);
@@ -48,7 +47,7 @@ impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
                     .asks
                     .iter() // cheapest first
                     .take_while(move |(ask, _)| *ask < &price)
-                    .flat_map(|(ask, xcs)| xcs.iter().map(move |(xc, q)| (ask, xc, q)));
+                    .flat_map(|(ask, xcs)| xcs.iter().map(move |(xc, q)| (xc, ask, q)));
                 Ok(Either::Left(arbitrages))
             }
             true => remove_price_from_exchange(&mut self.bids, price, exchange_id)
@@ -62,13 +61,7 @@ impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
         exchange_id: ExchangeIdT,
         price: PriceT,
         quantity: QuantityT,
-    ) -> Result<impl Iterator<Item = (&PriceT, &ExchangeIdT, &QuantityT)>, Error<ExchangeIdT>>
-    where
-        PriceT: Ord + Clone,
-        QuantityT: Zero,
-        ExchangeIdT: Eq + Hash,
-        BuildHasherT: BuildHasher + Default,
-    {
+    ) -> Result<impl Iterator<Item = (&ExchangeIdT, &PriceT, &QuantityT)>, Error<ExchangeIdT>> {
         match quantity.is_zero() {
             false => {
                 insert(&mut self.asks, price.clone(), exchange_id, quantity);
@@ -77,7 +70,7 @@ impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
                     .iter()
                     .rev() // most generous first
                     .take_while(move |(bid, _)| *bid > &price)
-                    .flat_map(|(bid, xcs)| xcs.iter().map(move |(xc, q)| (bid, xc, q)));
+                    .flat_map(|(bid, xcs)| xcs.iter().map(move |(xc, q)| (xc, bid, q)));
                 Ok(Either::Left(arbitrages))
             }
             true => remove_price_from_exchange(&mut self.bids, price, exchange_id)
@@ -137,7 +130,7 @@ where
 }
 
 impl<QuantityT, PriceT, ExchangeIdT, BuildHasherT> Default
-    for Arbitrage<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
+    for ArbitrageFinder<QuantityT, PriceT, ExchangeIdT, BuildHasherT>
 where
     BuildHasherT: Default,
 {
@@ -150,4 +143,46 @@ where
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::fmt::Debug;
+
+    use super::*;
+    use itertools::assert_equal;
+
+    type Finder<Q, P, E> = ArbitrageFinder<Q, P, E>;
+
+    #[test]
+    fn sell_highest_spread_first() {
+        let mut arbitrage = Finder::default();
+        assert_empty(arbitrage.buy("kraken", 10, 1).unwrap());
+        assert_empty(arbitrage.buy("kraken", 20, 1).unwrap());
+        assert_empty(arbitrage.buy("kraken", 30, 1).unwrap());
+        assert_empty(arbitrage.buy("kraken", 40, 1).unwrap());
+
+        assert_equal(
+            arbitrage.sell("binance", 20, 1).unwrap(),
+            [(&"kraken", &40, &1), (&"kraken", &30, &1)],
+        );
+    }
+
+    #[test]
+    fn buy_highest_spread_first() {
+        let mut arbitrage = Finder::default();
+        assert_empty(arbitrage.sell("kraken", 10, 1).unwrap());
+        assert_empty(arbitrage.sell("kraken", 20, 1).unwrap());
+        assert_empty(arbitrage.sell("kraken", 30, 1).unwrap());
+        assert_empty(arbitrage.sell("kraken", 40, 1).unwrap());
+
+        assert_equal(
+            arbitrage.buy("binance", 30, 1).unwrap(),
+            [(&"kraken", &10, &1), (&"kraken", &20, &1)],
+        );
+    }
+
+    fn assert_empty<T>(it: impl IntoIterator<Item = T>)
+    where
+        T: Debug + PartialEq,
+    {
+        assert_equal(it, iter::empty())
+    }
+}
