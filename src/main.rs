4a3229775070a83e74a3917440e32c671db326c9
Aatif Syed <aatifsyedyp@gmail.com>
2024-03-15 14:07:51 +0000

    feat: better logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{cmp, pin::pin};
 
-use futures::{stream, StreamExt, TryStreamExt};
+use clap::Parser;
+use futures::{stream, StreamExt as _, TryStreamExt as _};
 use openhedge_arbitrage::{
     integrations::{aevo, dydx, ExchangeMessage},
     ArbitrageFinder,
@@ -16,6 +17,13 @@ enum Exchange {
     Dydx,
 }
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    /// Omit `TRACE` logs
+    quiet: bool,
+}
+
 #[tokio::main]
 async fn main() {
     use tracing_subscriber::{
@@ -23,9 +31,15 @@ async fn main() {
         layer::SubscriberExt as _,
         util::SubscriberInitExt as _,
     };
+    let Args { quiet } = Args::parse();
     tracing_subscriber::fmt()
         .with_test_writer()
-        .with_max_level(LevelFilter::TRACE)
+        .with_max_level(match quiet {
+            true => LevelFilter::INFO,
+            false => LevelFilter::TRACE,
+        })
+        .without_time()
+        .with_target(false)
         .finish()
         .with(filter_fn(|it| {
             it.file().is_some_and(|file| file == file!())
@@ -44,30 +58,36 @@ async fn _main() {
     let mut balance = u32f32::ZERO;
     loop {
         let msg = messages.next().await;
-        trace!(?msg);
+        if let Some(Ok((exchange, message))) = &msg {
+            let (side, price, quantity) = match message {
+                ExchangeMessage::Buy { price, quantity } => ("buy", price, quantity),
+                ExchangeMessage::Sell { price, quantity } => ("sell", price, quantity),
+            };
+            trace!(?exchange, %side, %price, %quantity, "received message");
+        }
         match msg {
             None => unreachable!("stream always errors before terminating"),
             Some(Ok((exchange_id, msg))) => match msg {
                 ExchangeMessage::Buy { price, quantity } => {
-                    if let Ok(Some((_, sell_price, sell_quantity))) = finder
+                    if let Ok(Some((sell_exchange, sell_price, sell_quantity))) = finder
                         .buy(exchange_id, price, quantity)
                         .map(|mut it| it.next())
                     {
                         let spread = price - sell_price;
                         let quantity = cmp::min(quantity, *sell_quantity);
                         balance += spread * quantity;
-                        info!(%spread, %quantity, new_balance = %balance, "simulated arbitrage on buy");
+                        info!(%spread, %quantity, new_balance = %balance, buy = ?exchange_id, sell = ?sell_exchange, "simulated arbitrage");
                     };
                 }
                 ExchangeMessage::Sell { price, quantity } => {
-                    if let Ok(Some((_, buy_price, buy_quantity))) = finder
+                    if let Ok(Some((buy_exchange, buy_price, buy_quantity))) = finder
                         .sell(exchange_id, price, quantity)
                         .map(|mut it| it.next())
                     {
                         let spread = buy_price - price;
                         let quantity = cmp::min(quantity, *buy_quantity);
                         balance += spread * quantity;
-                        info!(%spread, %quantity, new_balance = %balance, "simulated arbitrage on sell");
+                        info!(%spread, %quantity, new_balance = %balance, sell = ?exchange_id, buy = ?buy_exchange, "simulated arbitrage");
                     };
                 }
             },
